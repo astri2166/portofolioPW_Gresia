8ff6580333c05d56745579b9e68416ed520e0f7a
astri2166 <astriananta176@gmail.com>
2025-11-19 22:30:59 +0700

    initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Information Systems Student", "UI/UX Enthusiast", "Canva Creator"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
